bdcb1c29e55b3f1a3da10bbff132814967500a3a
leolin16 <leolin16@msn.com>
2019-09-26 21:57:29 +0800

    second commit

--- Globomantics/Startup.cs
+++ Globomantics/Startup.cs
@@ -17,6 +17,7 @@ namespace Globomantics
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddSingleton<IConferenceService, ConferenceMemoryService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
